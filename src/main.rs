d81524a8426a661a3eed5501dcc2565a3de40305
AchroDev <ztblaine@protonmail.com>
2024-03-24 00:21:36 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    println!();
+
+    let name:&str = "AchroDev";
+    println!("First program in Rust, let's go baby!");
+    println!("This application was developed by ~~{name}~~");
+}
